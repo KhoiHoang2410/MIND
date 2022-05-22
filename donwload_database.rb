2df655cf031fff684b8dfe87d29af1ee378ed478
KhoiHoang2410 <khoi2410@gmail.com>
2022-05-22 22:01:55 +0700

    remove redundance

--- donwload_database.rb
+++
@@ -1,5 +0,0 @@
-require 'open-uri'
-
-File.open('mind_development.sql', 'w') do |stream|
-  stream << URI.open('https://drive.google.com/uc?export=download&id=1B4FEYdqaAQfsTqkzXgq-NhVD8ybIri0-').read
-end
